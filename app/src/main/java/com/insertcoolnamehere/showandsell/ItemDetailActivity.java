700c86a6cee97572dc394a1fc8dc36b23673486d
Adel Hassan <sean.dashals@gmail.com>
2016-11-22 11:19:06 -0600

    v0.9.4 * put together a skeleton for item comments

--- app/src/main/java/com/insertcoolnamehere/showandsell/ItemDetailActivity.java
+++ app/src/main/java/com/insertcoolnamehere/showandsell/ItemDetailActivity.java
@@ -1,11 +1,21 @@
 package com.insertcoolnamehere.showandsell;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ImageView;
 
+import com.insertcoolnamehere.showandsell.dummy.DummyContent;
 import com.insertcoolnamehere.showandsell.logic.Item;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -35,5 +45,16 @@ public class ItemDetailActivity extends AppCompatActivity {
         itemCondition.setText(mItem.getCondition());
         TextView itemDescription = (TextView) findViewById(R.id.item_detail_description);
         itemDescription.setText(mItem.getDescription());
+
+        // set up comments list view
+        ListView listView = (ListView) findViewById(R.id.item_comments);
+        ArrayList<String> list = new ArrayList<String>();
+        ListIterator<DummyContent.DummyItem> iter = DummyContent.ITEMS.listIterator();
+        while(iter.hasNext()) {
+            DummyContent.DummyItem item = iter.next();
+            list.add(item.content);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.text_view_comment, R.id.textView_comment, list);
+        listView.setAdapter(adapter);
     }
 }
