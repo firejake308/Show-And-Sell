c3391fe6cf01bbfd300aec09ca11951339939892
Adel Hassan <sean.dashals@gmail.com>
2017-02-11 23:30:28 -0600

    v1.1.5 * Customized input for each step of donate item

--- app/src/main/java/com/insertcoolnamehere/showandsell/DonateActivity.java
+++ app/src/main/java/com/insertcoolnamehere/showandsell/DonateActivity.java
@@ -1,20 +1,30 @@
 package com.insertcoolnamehere.showandsell;
 
 import android.content.Context;
+import android.content.res.XmlResourceParser;
 import android.database.DataSetObserver;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class DonateActivity extends AppCompatActivity {
@@ -92,11 +102,55 @@ public class DonateActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         mCurrentStep++;
-                        View editText = findViewById(R.id.step_text_entry);
-                        editText.getParent().clearChildFocus(editText);
+                        View editText = findViewById(R.id.item_description_entry);
+                        if (editText != null)
+                            editText.getParent().clearChildFocus(editText);
                         mListView.invalidateViews();
                     }
                 });
+
+                // configure input view
+                LinearLayout flex = (LinearLayout) finalProduct.findViewById(R.id.step_input_parent);
+                switch(mCurrentStep) {
+                    case 0:
+                        // create an imageview
+                        ImageView uploadImg = (ImageView) getLayoutInflater().inflate(R.layout.upload_img_view, flex, false);
+
+                        // create the explanatory textview
+                        TextView instructions = new TextView(getApplicationContext());
+                        instructions.setText(R.string.prompt_confirm_password);
+                        instructions.setTextColor(getColor(R.color.hintOrDisabledText));
+
+                        flex.addView(instructions);
+                        flex.addView(uploadImg);
+
+                        // TODO update connector length
+                        //View connector = finalProduct.findViewById(R.id.step_connector);
+                        //connector.setMinimumHeight(184);
+                        break;
+                    case 1:
+                        // inflate EditText for title entry
+                        View titleEntry = getLayoutInflater().inflate(R.layout.item_title_entry, flex, false);
+                        flex.addView(titleEntry);
+                        break;
+                    case 2:
+                        View detailsCustom = getLayoutInflater().inflate(R.layout.item_details_entry, flex, false);
+                        flex.addView(detailsCustom);
+                        break;
+                    case 3:
+                        View priceCustom = getLayoutInflater().inflate(R.layout.item_price_entry, flex, false);
+                        flex.addView(priceCustom);
+                        break;
+                    case 4:
+                        View conditionCustom = getLayoutInflater().inflate(R.layout.item_condition_entry, flex, false);
+                        Spinner conditionEntry = (Spinner) conditionCustom.findViewById(R.id.item_condition_entry);
+                        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(),
+                                R.array.possible_conditions, android.R.layout.simple_spinner_item);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        conditionEntry.setAdapter(adapter);
+                        flex.addView(conditionCustom);
+                        break;
+                }
             }
 
             // remove connector line for last step
